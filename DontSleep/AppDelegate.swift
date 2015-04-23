4addef776e15f38e64829b98e64a05169e7e9d44
Mitch Winn <mitchwinn@Mitchs-MacBook-Pro.local>
2015-04-22 21:33:56 -0700

    Signed-off-by: Mitch Winn <mitchwinn@Mitchs-MacBook-Pro.local>
    Initial commit for DontSleep project

--- DontSleep/AppDelegate.swift
+++ DontSleep/AppDelegate.swift
@@ -12,16 +12,30 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    var statusItem = NSStatusItem()
+    var darkModeOn = false
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // Create the new status item with a certain alloated space.
+        // Magic number "-1" stands for NSVariableStatusItemLength. A bug in beta 3
+        // caused a linker error due to NSVariableStatusItemLength chaning to CGFloat.
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+        
+        // Add an image to the statusItem.
+        self.statusItem.image = NSImage(named: "sunny24")
+        
+        // Create a action when the item is clicked.
+        self.statusItem.action = Selector("itemClicked:")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    func itemClicked(sender: AnyObject) {
+        // Change the statusItem image.
+        self.statusItem.image = NSImage(named: "sunny24")
+    }
 
 }
 
