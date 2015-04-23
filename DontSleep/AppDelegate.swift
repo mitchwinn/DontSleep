cdb512c7dc1c5cad5ee8f68f1fc6b32cf16aa50f
Mitch Winn <mitchwinn@Mitchs-MacBook-Pro.local>
2015-04-22 20:50:35 -0700

    Initial Commit

---
+++ DontSleep/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DontSleep
+//
+//  Created by Mitch Winn on 4/22/15.
+//  Copyright (c) 2015 Mitch Winn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
